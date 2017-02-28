32e88568d45ec0ab4b822f970c662c48b70484a2
Samantha Young <samantha.young@students.olin.edu>
2017-02-28 15:14:27 -0500

    Toolbox 1: word frequency

--- frequency.py
+++ frequency.py
@@ -1,7 +1,7 @@
-""" Analyzes the word frequencies in a book downloaded from
-Project Gutenberg """
-
 import string
+import requests
+
+beautiful_text = requests.get('http://www.gutenberg.org/cache/epub/9830/pg9830.txt').text
 
 
 def get_word_list(file_name):
@@ -10,7 +10,17 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    exclude = set(string.punctuation)  # makes set of punctuation characters
+    file1 = file_name
+    s = ''.join(ch for ch in file1 if ch not in exclude)   # for each character of the story creates a new string without any of the pucntiation
+    startBook = s.index('CHAPTER I') + 9
+    s = s.lower()
+    s = s[startBook:]
+    lis_File = s.split() # breaks up string at the spaces creates a list of elements
+    return lis_File
+
+
+get_word_list(beautiful_text)
 
 
 def get_top_n_words(word_list, n):
@@ -23,8 +33,15 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    words = word_list
+    word_freq = dict()
+    for c in words:
+        word_freq[c] = word_freq.get(c, 0) + 1  # counts the number of times eeach word appears and keeps track in dictioary
+    freqsorted = sorted(word_freq, key=word_freq.__getitem__, reverse=True)  # sorts dictionary by value (by frequency of word in story) highest value first
+    print(freqsorted[:n])
+    return freqsorted[:n]  # returns the first 10 words
+
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    get_top_n_words(get_word_list(beautiful_text), 100)
